039037166e2b82f92d276383814c52d916676467
Zhenya Sayapin <ep.sayapin@gmail.com>
2026-01-16 23:07:15 +0400

    Update validation

--- src/DTO/CalculatePriceDTO.php
+++ src/DTO/CalculatePriceDTO.php
@@ -6,13 +6,15 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class CalculatePriceDTO
 {
-    #[Assert\NotBlank(message: 'You must provide a product id')]
+    #[Assert\NotBlank(message: 'Product id is required')]
     #[Assert\Positive]
     public int $product;
 
     #[Assert\Length(max: 255)]
+    #[Assert\Regex(pattern: '/^[A-Z]{2,4}[0-9]{9,11}$/', message: 'Provided tax number has invalid format')]
     public ?string $taxNumber = null;
 
     #[Assert\Length(max: 255)]
+    #[Assert\Regex(pattern: '/^[A-Z][0-9]+$/', message: 'Provided coupon has invalid format')]
     public ?string $couponCode = null;
 }
